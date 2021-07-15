4e4f878214d903080333234644e09459dc2b37a1
MaifiiDE <76437533+MaifiiDE@users.noreply.github.com>
2021-07-15 17:59:32 +0200

    init commands and listeners

--- src/de/maifii/lobby/main/Lobby.java
+++ src/de/maifii/lobby/main/Lobby.java
@@ -1,7 +1,11 @@
 package de.maifii.lobby.main;
 
+import de.maifii.lobby.commands.BuildCommand;
+import de.maifii.lobby.commands.SetCommand;
+import de.maifii.lobby.listeners.*;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
@@ -18,17 +22,26 @@ public class Lobby extends JavaPlugin {
     private static ArrayList<Player> HerzPartikel;
 
 
+    public void register(PluginManager pluginManager) {
+        pluginManager.registerEvents(new BlockListener(), this);
+        pluginManager.registerEvents(new GUI(), this);
+        pluginManager.registerEvents(new MoveListener(), this);
+        pluginManager.registerEvents(new ProtectListener(), this);
+        pluginManager.registerEvents(new ConnectionListener(), this);
+
+        getCommand("set").setExecutor(new SetCommand());
+        getCommand("build").setExecutor(new BuildCommand());
+    }
+
     @Override
     public void onEnable() {
+        this.register(Bukkit.getPluginManager());
+
         instance = instance;
         WasserPartikel = new ArrayList<Player>();
         EnderPartikel = new ArrayList<Player>();
         HerzPartikel = new ArrayList<Player>();
         BuildMode = new ArrayList<Player>();
-
-
-
-
     }
 
     @Override
